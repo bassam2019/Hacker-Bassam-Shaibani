111519e316a43bd1921f03c01a3ef960ce43ac9a
bassam2019 <54856742+bassam2019@users.noreply.github.com>
2019-09-05 17:54:17 +0300

    Create control.py

---
+++ control.py
@@ -0,0 +1,8 @@
+# Welcome to the Kingdom of Hacker Bassam Shaibani
+
+
+
+echo -e $green"--------------------------------------------------------------------------
+sleep 0.3
+echo -e $red                          Hacker Bassam Shaibani
+sleep 0.5
