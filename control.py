4f9718fae717b5e879aab58b52e1a722208868c4
bassam2019 <54856742+bassam2019@users.noreply.github.com>
2019-09-06 17:22:36 +0300

    Update control.py

--- control.py
+++ control.py
@@ -1,8 +1,13 @@
 # Welcome to the Kingdom of Hacker Bassam Shaibani
 
-
-
-echo -e $green"--------------------------------------------------------------------------
+red='\e[1;31m'
+green='\e[1;32m'
+blue='\e[1;34m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+clear
+echo -e $green "--------------------------------------------------------------------------"
 sleep 0.3
-echo -e $red                          Hacker Bassam Shaibani
-sleep 0.5
+echo -e $red "                          Hacker Bassam Shaibani                            "
